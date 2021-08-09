96edd3cf8dd5cfbbf8021fc3ae7c5cbcf1bfae14
AlienGao <385184602@qq.com>
2021-08-09 15:40:27 +0800

    init

--- docs/.vuepress/config/plugins.js
+++ docs/.vuepress/config/plugins.js
@@ -83,11 +83,11 @@ module.exports = [
     {
       choosen: 'gitalk',
       options: {
-        clientID: 'a6e1355287947096b88b',
-        clientSecret: 'f0e77d070fabfcd5af95bebb82b2d574d7248d71',
+        clientID: 'ce6598e0de3e7a4afde8',
+        clientSecret: '172e8e7cb1e46da42ab9ae145088f72e6878af90',
         repo: 'blog-gitalk-comment', // GitHub 仓库
-        owner: 'xugaoyi', // GitHub仓库所有者
-        admin: ['xugaoyi'], // 对仓库有写权限的人
+        owner: 'Aliengao', // GitHub仓库所有者
+        admin: ['Aliengao'], // 对仓库有写权限的人
         // distractionFreeMode: true,
         pagerDirection: 'last', // 'first'正序 | 'last'倒序
         id: '<%- (frontmatter.permalink || frontmatter.to.path).slice(-16) %>', //  页面的唯一标识,长度不能超过50
