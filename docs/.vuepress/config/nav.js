986f8937252bb88abd6381b8983427e849eea5ff
AlienGao <385184602@qq.com>
2021-08-11 21:54:36 +0800

    qiankun应用

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -1,49 +1,49 @@
 // nav
 module.exports = [
-  { text: '首页', link: '/' },
+  { text: "首页", link: "/" },
   {
-    text: '前端',
-    link: '/web/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
+    text: "前端",
+    link: "/web/", //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
     items: [
       // 说明：以下所有link的值只是在相应md文件定义的永久链接（不是什么特殊生成的编码）。另外，注意结尾是有斜杠的
       {
-        text: '前端文章',
-        link: '/pages/c1947e/',
+        text: "前端文章",
+        link: "/pages/c1947e/",
         items: [
-          { text: 'JavaScript', link: '/pages/c1947e/' },
+          { text: "JavaScript", link: "/pages/c1947e/" },
           // { text: 'Vue', link: '/pages/5d463fbdb172d43b/' },
         ],
       },
-      {
-        text: '学习笔记',
-        items: [
-          { text: '《ES6 教程》笔记', link: '/note/es6/' },
-          { text: '《Vue》笔记', link: '/note/vue/' },
-          { text: '《React》笔记', link: '/note/react/' },
-        ],
-      },
+      // {
+      //   text: "学习笔记",
+      //   items: [
+      //     { text: "《ES6 教程》笔记", link: "/note/es6/" },
+      //     { text: "《Vue》笔记", link: "/note/vue/" },
+      //     { text: "《React》笔记", link: "/note/react/" },
+      //   ],
+      // },
     ],
   },
   {
-    text: '算法',
-    link: '/leetcode/',
+    text: "算法",
+    link: "/leetcode/",
     items: [
-      { text: '贪心', link: '/leetcode/greed/' },
-      { text: '广度优先/深度优先', link: '/leetcode/dfs/' },
+      { text: "贪心", link: "/leetcode/greed/" },
+      { text: "广度优先/深度优先", link: "/leetcode/dfs/" },
     ],
   },
   {
-    text: '技术',
-    link: '/technology/',
+    text: "技术",
+    link: "/technology/",
     items: [
-      // { text: '技术文档', },
-      { text: 'GitHub技巧', link: '/pages/1e9c2d/' },
+      { text: "技术文档", link: "/pages/ee319a/" },
+      { text: "GitHub技巧", link: "/pages/1e9c2d/" },
       // { text: '博客搭建', },
     ],
   },
   {
-    text: '收藏',
-    link: '/pages/beb6c0bd8a66cea6/',
+    text: "收藏",
+    link: "/pages/beb6c0bd8a66cea6/",
     // items: [
     //   { text: '网站', link: '/pages/beb6c0bd8a66cea6/' },
     //   { text: '资源', link: '/pages/eee83a9211a70f9d/' },
@@ -51,12 +51,12 @@ module.exports = [
     // ],
   },
   {
-    text: '索引',
-    link: '/archives/',
+    text: "索引",
+    link: "/archives/",
     items: [
-      { text: '分类', link: '/categories/' },
-      { text: '标签', link: '/tags/' },
-      { text: '归档', link: '/archives/' },
+      { text: "分类", link: "/categories/" },
+      { text: "标签", link: "/tags/" },
+      { text: "归档", link: "/archives/" },
     ],
   },
-]
+];
