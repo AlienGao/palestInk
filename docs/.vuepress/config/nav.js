c6173ed16e77bd074f69f5f6c7ba4758751600cc
AlienGao <385184602@qq.com>
2021-08-14 19:30:57 +0800

    update

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -6,14 +6,9 @@ module.exports = [
     link: "/web/", //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
     items: [
       // 说明：以下所有link的值只是在相应md文件定义的永久链接（不是什么特殊生成的编码）。另外，注意结尾是有斜杠的
-      {
-        text: "前端文章",
-        link: "/pages/c1947e/",
-        items: [
-          { text: "JavaScript", link: "/pages/c1947e/" },
-          // { text: 'Vue', link: '/pages/5d463fbdb172d43b/' },
-        ],
-      },
+      { text: "JavaScript", link: "/frontend/js/" },
+      { text: "Antd组件", link: "/frontend/component/" },
+      // { text: 'Vue', link: '/pages/5d463fbdb172d43b/' },
       // {
       //   text: "学习笔记",
       //   items: [
