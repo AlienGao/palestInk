44371c60dd84b8c2c25b901c2f659a8bc31cfb53
AlienGao <385184602@qq.com>
2021-08-18 23:19:44 +0800

    deploy

---
+++ public.js
@@ -0,0 +1,19 @@
+const git = require("simple-git");
+const path = "docs/.vuepress/dist";
+const repo = "git@github.com:AlienGao/palestInk.git";
+
+git(path)
+  .init()
+  .add("./*")
+  .commit("public")
+  .addRemote("origin", repo)
+  .push(["-f", "origin", "master"], () => {
+    console.log("Push to master success");
+  })
+  .checkoutLocalBranch("gh-pages", () => {
+    console.log("Checkout to branch gh-pages");
+  })
+  .push(["-f", "origin", "gh-pages"])
+  .checkout("master", () => {
+    console.log("Finish public, back to branch master.");
+  });
