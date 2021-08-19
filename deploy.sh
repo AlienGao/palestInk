6f637043ef459803187f5a3fab864d4b8f49d947
AlienGao <385184602@qq.com>
2021-08-19 23:22:41 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -4,26 +4,26 @@
 set -e
 
 # 生成静态文件
-yarn build
+# yarn build
 
 # 进入生成的文件夹
-cd docs/.vuepress/dist
+# cd docs/.vuepress/dist
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
 
-git init
-git add -A
-git commit -m 'deploy'
+# git init
+# git add -A
+# git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:AlienGao/AlienGao.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:AlienGao/palestInk.git master:gh-pages
+# git push -f git@github.com:AlienGao/palestInk.git master:gh-pages
 
-cd -
-rm -rf docs/.vuepress/dist
+# cd -
+# rm -rf docs/.vuepress/dist
 
 git add -A
 git commit -m 'deploy'
